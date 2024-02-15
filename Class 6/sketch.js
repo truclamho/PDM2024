8c2134d50c50e6c28e699b22ca93e4b8f1cca4e9
Truc Lam Ho <tho12@lsu.edu>
2024-02-15 16:15:12 -0600

    Sprite Animation Assignment

---
+++ Class 6/sketch.js
@@ -0,0 +1,91 @@
+let sprite;
+
+function preload() {
+  sprite = new Sprite(200, 200, 80, 80);
+  sprite.spriteSheet = 'assets/ninja.png';
+  let animations = {
+    stand: { row: 0, frames: 1 },
+    walkRight: { row: 0, col: 1, frames: 8 },
+    walkUp: { row: 5, frames: 6},
+    walkDown: { row: 5, col: 6, frames: 6}
+  };
+  sprite.anis.frameDelay = 8;
+  sprite.addAnis(animations);
+  sprite.changeAni('walkRight');
+}
+
+function setup() {
+  createCanvas(400, 400);
+
+}
+
+function draw() {
+  background(0);
+
+  if (keyIsDown('d')) {
+    walkRight();
+  } else if (kb.pressing('a')) {
+    walkLeft();
+  } else if (kb.pressing('w')) {
+    walkUp();
+  } 
+  else if (kb.pressing('s')) {
+    walkDown();
+  }
+  else {
+    stop();
+  }
+
+  if(sprite.x + sprite.width/4 > width) {
+    walkLeft();
+  } else if (sprite.x - sprite.width/4 < 0){
+    walkRight();
+  }
+}
+
+function stop() {
+  sprite.vel.x = 0;
+  sprite.vel.y = 0;
+  sprite.changeAni('stand');
+}
+
+function walkRight() {
+  sprite.changeAni('walkRight');
+      sprite.vel.x = 1;
+      sprite.scale.x = 1;
+      sprite.vel.y = 0;
+}
+
+function walkLeft() {
+  sprite.changeAni('walkRight');
+      sprite.vel.x = -1;
+      sprite.scale.x = -1;
+      sprite.vel.y = 0;
+}
+
+function walkUp() {
+  sprite.changeAni('walkUp');
+      sprite.vel.y = -1;
+      sprite.vel.x = 0;
+}
+
+function walkDown() {
+  sprite.changeAni('walkDown');
+      sprite.vel.y = 1;
+      sprite.vel.x = 0;
+}
+
+function keyTypedOld() {
+  switch(key) {
+    case 'd':
+      walkRight();
+      break;
+    case 'a':
+      walkLeft();
+      break;
+    case 'w':
+      break;
+    case 's':
+      break;
+  }
+}
---
+++ Class 7/sketch.js
@@ -0,0 +1,110 @@
+let sprite;
+let characters = [];
+
+function preload() {
+  let animations = {
+    stand: { row: 0, frames: 1 },
+    walkRight: { row: 0, col: 1, frames: 8 },
+    walkUp: { row: 5, frames: 6},
+    walkDown: { row: 5, col: 6, frames: 6}
+  };
+
+  characters.push(new Character(100,100,80,80,'assets/ninja.png', animations));
+  characters.push(new Character(200,200,80,80,'assets/ninja.png', animations));
+}
+
+function setup() {
+  createCanvas(400, 400);
+
+}
+
+function draw() {
+  background(0);
+
+  characters.forEach((character) => {
+    if (kb.pressing('d')) {
+      character.walkRight();
+    } else if (kb.pressing('a')) {
+      character.walkLeft();
+    } else if (kb.pressing('w')) {
+      character.walkUp();
+    } 
+    else if (kb.pressing('s')) {
+      character.walkDown();
+    }
+    else {
+      character.stop();
+    }
+
+      if(character.sprite.x + character.sprite.width/4 > width) {
+    character.walkLeft();
+  } else if (character.sprite.x - character.sprite.width/4 < 0){
+    character.walkRight();
+  }
+  })
+  
+
+  // if(sprite.x + sprite.width/4 > width) {
+  //   walkLeft();
+  // } else if (sprite.x - sprite.width/4 < 0){
+  //   walkRight();
+  // }
+}
+
+class Character {
+  constructor(x,y,width,height,spriteSheet,animations) {
+    this.sprite = new Sprite(x,y,width,height);
+    this.sprite.spriteSheet = spriteSheet;
+    
+    this.sprite.anis.frameDelay = 8;
+    this.sprite.addAnis(animations);
+    this.sprite.changeAni('stand');
+  }
+
+   stop() {
+    this.sprite.vel.x = 0;
+    this.sprite.vel.y = 0;
+    this.sprite.changeAni('stand');
+  }
+  
+   walkRight() {
+    this.sprite.changeAni('walkRight');
+    this.sprite.vel.x = 1;
+    this.sprite.scale.x = 1;
+    this.sprite.vel.y = 0;
+  }
+  
+   walkLeft() {
+    this.sprite.changeAni('walkRight');
+    this.sprite.vel.x = -1;
+    this.sprite.scale.x = -1;
+    this.sprite.vel.y = 0;
+  }
+  
+   walkUp() {
+    this.sprite.changeAni('walkUp');
+    this.sprite.vel.y = -1;
+    this.sprite.vel.x = 0;
+  }
+  
+   walkDown() {
+    this.sprite.changeAni('walkDown');
+    this.sprite.vel.y = 1;
+    this.sprite.vel.x = 0;
+  }
+}
+
+function keyTypedOld() {
+  switch(key) {
+    case 'd':
+      walkRight();
+      break;
+    case 'a':
+      walkLeft();
+      break;
+    case 'w':
+      break;
+    case 's':
+      break;
+  }
+}
