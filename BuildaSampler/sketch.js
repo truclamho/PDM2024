b11697966dc0320d4dea9ea2ffebd125d0a13217
Truc Lam Ho <tho12@lsu.edu>
2024-02-27 18:11:31 -0600

    Build a Sampler

--- BuildaSampler/sketch.js
+++ BuildaSampler/sketch.js
@@ -20,26 +20,27 @@ sounds.toDestination();
 function setup() {
   createCanvas(400, 400);
 
-  button1 = createButton ('Beach Sound');
-  button1.position (80, 100);
+  button1 = createButton('Beach Sound');
+  button1.position(80, 100);
   button1.mousePressed(() => sounds.player('beach').start());
 
-  button2 = createButton ('Moorland Sound');
-  button2.position (190, 100);
+  button2 = createButton('Moorland Sound');
+  button2.position(190, 100);
   button2.mousePressed(() => sounds.player('moorland').start());
 
-  button3 = createButton ('Rain Sound');
-  button3.position (85, 150);
+  button3 = createButton('Rain Sound');
+  button3.position(85, 150);
   button3.mousePressed(() => sounds.player('rain').start());
 
-  button4 = createButton ('Stream Sound');
-  button4.position (195, 150);
+  button4 = createButton('Stream Sound');
+  button4.position(195, 150);
   button4.mousePressed(() => sounds.player('stream').start());
 
-  pitchSlider = createSlider (-48, 48, 0, 0.01);
-  pitchSlider.position (140, 260);
-  pitchSlider.mouseMoved (() => pitchShift.pitch = pitchSlider.value);
+  pitchSlider = createSlider(-48, 48, 0, 0.01);
+  pitchSlider.position(140, 260);
+  pitchSlider.mouseMoved(() => pitchShift.pitch = pitchSlider.value);
 }
+
 
 function draw() {
   background(147, 182, 237);
